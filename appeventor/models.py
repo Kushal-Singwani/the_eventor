8996d7f212f7d96ad7a7f31b7759c584082011ff
kushalsingwani <priyeshpandey0705@gmail.com>
2025-04-16 17:59:27 +0530

    commit

--- appeventor/models.py
+++ appeventor/models.py
@@ -23,8 +23,9 @@ class adminuser(models.Model):
 
 
 class FakePayment(models.Model):
-    # user = models.ForeignKey(datadesire, on_delete=models.CASCADE, related_name="payments") 
+    user = models.ForeignKey(datadesire, on_delete=models.CASCADE, related_name="payments",null=True) 
     name = models.CharField(max_length=100)
+    event=models.CharField(max_length=200,null=True)
     email = models.EmailField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     transaction_id = models.CharField(max_length=20, unique=True)
